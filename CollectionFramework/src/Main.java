80dc76d9593dc5f10ad033fa5ebbc45d9460cfc2
Nomvula <nomvula.skosana@xgileit.co.za>
2021-07-30 18:01:54 +0200

    Collections Framework

---
+++ CollectionFramework/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+// I have chosen to use LinkedHasSet data structure because:
+        //1. It does not allow duplicates
+        //2. I can delete an object that is in any position
+
+        LinkedHashSet<Student> ls = new LinkedHashSet<Student>();
+
+        //Creating students
+       Student student1 = new Student("Nomvula", "Skosana", "555000", 'A', 'F', "IT");
+       Student student2 = new Student("Portia", "Klark", "222000", 'B', 'F', "Web dev");
+       Student student3 = new Student("Linda", "Mahlangu", "333000", 'D', 'M', "Analyst");
+       Student student4 = new Student("Kilo", "Beith", "444000", 'C', 'M', "BA");
+       Student student5 = new Student("Bella", "Creg", "111000", 'E', 'F', "SA");
+
+        //Adding student to a hash table
+        ls.add(student1);
+        ls.add(student2);
+        ls.add(student3);
+        ls.add(student4);
+        ls.add(student5);
+        System.out.println("----------------------Printing all students------------------------\n");
+
+        //List all Students
+        Iterator<Student> itr= ls.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        //Removing student1
+        ls.remove(student1);
+
+        System.out.println("\n-------------------Printing remaining students after removing one---------------\n");
+        Iterator<Student> itr2= ls.iterator();
+        while(itr2.hasNext()) {
+            System.out.println(itr2.next());
+        }
+
+        //I have chosen to use TreeSet here because:
+        //1. I can sort according to any field that I want, in this case, the grade
+        //2. I can find the greatest or lowest element in the set
+        System.out.println("\n-------------------Sorted According to Grade--------------------\n");
+        TreeSet<Student> ts=new TreeSet<Student>();
+        ts.add(student1);
+        ts.add(student2);
+        ts.add(student3);
+        ts.add(student4);
+        ts.add(student5);
+
+        Iterator<Student> itr3= ts.iterator();
+
+        while(itr3.hasNext()) {
+            System.out.println(itr3.next());
+        }
+
+        System.out.println("\n-------------------Printing student with Grade 'B'-------------------\n");
+        Iterator<Student> itr4= ts.iterator();
+
+        while(itr4.hasNext()) {
+            Student next = itr4.next();
+            if ((next.getGrade().equals('B'))){
+                System.out.println(next);
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ CollectionFramework/src/Student.java
@@ -0,0 +1,97 @@
+import java.util.Objects;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private String surname;
+    private String id;
+    private Character grade;
+    private char gender;
+    private String courseName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Character getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Character grade) {
+        this.grade = grade;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id='" + id + '\'' +
+                ", grade=" + grade +
+                ", gender=" + gender +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
+
+    public Student(String name, String surname, String id, char grade, char gender, String courseName) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.grade = grade;
+        this.gender = gender;
+        this.courseName = courseName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getGrade() == student.getGrade() && getGender() == student.getGender() && Objects.equals(getName(), student.getName()) && Objects.equals(getSurname(), student.getSurname()) && Objects.equals(getId(), student.getId()) && Objects.equals(getCourseName(), student.getCourseName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurname(), getId(), getGrade(), getGender(), getCourseName());
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.grade.compareTo(o.getGrade());
+    }
+}
